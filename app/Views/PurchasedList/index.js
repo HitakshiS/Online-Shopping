cab0d93bdc832bebecd62670a32e460c197892e4
Hitakshi Soma <hitakshi@atlogys.com>
2019-07-04 10:40:31 +0530

    purchase list handled

--- app/Views/PurchasedList/index.js
+++ app/Views/PurchasedList/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { View, StyleSheet, FlatList, CustomText } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 import { connect } from "react-redux";
 import ListItem from "../../Components/ListItem";
+import CustomText from "../../Components/CustomText";
 
 class PurchasedList extends Component {
   static navigationOptions = {
@@ -23,24 +24,27 @@ class PurchasedList extends Component {
       this.props.reducer.purchaseList,
       this.props.reducer.exampleData
     );
+
+    console.log(purchaseData);
     return (
-      <View>
-        {this.props.reducer.purchaseList.length > -1 ? (
+      <View style={{ flex: 1 }}>
+        {purchaseData && purchaseData.length > 0 ? (
           <FlatList
             data={purchaseData}
             horizontal={this.props.horizontal}
             renderItem={this.renderItem}
           />
         ) : (
-          <CustomText
-            style={{
-              fontSize: 20,
-              color: "#7a42f4",
-              flex: 1,
-              textAlign: "center"
-            }}
-            title="You have not ordered anything yet!!"
-          />
+          !this.props.horizontal && (
+            <CustomText
+              style={{
+                fontSize: 20,
+                color: "#7a42f4",
+                textAlign: "center"
+              }}
+              title="You have not ordered anything yet!!"
+            />
+          )
         )}
       </View>
     );
